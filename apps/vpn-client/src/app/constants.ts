4f43d5effd15a2d64786e04b5f0b0158fe09ffb2
Allen OYANG <allenwill211@gmail.com>
2022-08-24 09:50:39 +0800

    first commit

---
+++ apps/vpn-client/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'vpn-client';
+export const electronAppName = 'vpn-client';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
